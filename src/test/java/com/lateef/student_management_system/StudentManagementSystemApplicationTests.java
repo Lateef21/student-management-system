7b336a46f5997eb2ec145db05966f6274f28ce49
Abdullateef Edun <lateefedun21@gmail.com>
2025-06-25 11:29:34 +0100

    Initial commit

---
+++ src/test/java/com/lateef/student_management_system/StudentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lateef.student_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
